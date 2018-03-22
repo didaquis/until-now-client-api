420a40ebd6ef874a7ce9d1b611d312164e987d86
Dídac García <didaquis@gmail.com>
2018-03-22 22:02:43 +0100

    :books: Documentada la API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 /**
- * untilNowApi client API
+ * untilNowApi API client
  * 
  */
 const rp = require('request-promise');
 
+/**
+ *  Until Now API client
+ */
 const untilNowApi = {
+	/**
+	 * @returns Main URL of endpoint
+	 */
 	_baseUrl() {
 		return `${this.protocol}://${this.host}:${this.port}`;
 	},
 
+	/**
+	 * 
+	 * @param {string} method - Method of petition: get, post...
+	 * @param {string} path - precise endpoint
+	 * @param {string} [body] - body of petition
+	 * @returns {Promise}
+	 */
 	_call(method, path, body) {
 		return rp({
 			method,
@@ -18,26 +31,54 @@ const untilNowApi = {
 		});
 	},
 
+	/**
+	 * Send ping to API server
+	 * @returns {Promise}
+	 */
 	ping(){
 		return this._call('get','api/ping');
 	},
 
+	/**
+	 * Request all collections
+	 * @returns {Promise}
+	 */
 	listCollections(){
 		return this._call('get','api/collections');
 	},
 
+	/**
+	 * Request a collection
+	 * @param {string} id - Id of collection
+	 * @returns {Promise}
+	 */
 	retrieveCollection(id){
 		return this._call('get',`api/collection/${id}`);
 	},
 
+	/**
+	 * Request all items
+	 * @returns {Promise}
+	 */
 	listItems(){
 		return this._call('get', 'api/items');
 	},
 
+	/**
+	 * Request items in a collection
+	 * @param {string} id - Id of collection
+	 * @returns {Promise}
+	 */
 	listItemsInCollection(id){
 		return this._call('get', `api/items/${id}`);
 	},
 
+	/**
+	 * Create a new collection
+	 * @param {string} name - Name of collection
+	 * @param {string} id_user - Id of username owner
+	 * @returns {Promise}
+	 */
 	createCollection(name, id_user){
 		const body = {
 			"name": name,
@@ -46,6 +87,16 @@ const untilNowApi = {
 		return this._call('post','api/collection', body);
 	},
 
+	/**
+	 * Create a new item
+	 * @param {string} name - Name of item 
+	 * @param {string} dateStart - Must be format as valid Date object
+	 * @param {string} dateEnd - Must be format as valid Date object
+	 * @param {string} [refNumber] 
+	 * @param {string} [notes] 
+	 * @param {string} id_collection 
+	 * @returns {Promise}
+	 */
 	createItem(name, dateStart, dateEnd, refNumber, notes, id_collection){
 		const body = {
 			"name": name,
@@ -58,21 +109,24 @@ const untilNowApi = {
 		return this._call('post','api/item', body);
 	},
 
+	/**
+	 * Delete a collection
+	 * @param {string} id - Id of collection
+	 * @returns {Promise}
+	 */
 	deleteCollection(id){
 		return this._call('delete', `api/collection/${id}`);
 	},
 
+	/**
+	 * Delete an item
+	 * @param {string} id - Id of item
+	 * @returns {Promise}
+	 */
 	deleteItem(id){
 		return this._call('delete', `api/item/${id}`);
 	}
 
-	// loginUser(username, password){
-	// 	const body = {
-	// 		"username": username,
-	// 		"password": password
-	// 	};
-	// 	return this._call('post', `api/user`, body);
-	// }
 };
 
 module.exports = untilNowApi;
